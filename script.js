9337bd616cd18360ace203d0ca1f4b8e513564a0
Vladislav Striga <vstriga.exceedteam@gmail.com>
2021-05-18 18:22:40 +0300

    v3

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let itemList = document.getElementById("listname");
-let inputForm = document.getElementById("i_form");
+const itemList = document.getElementById("listname");
+const inputForm = document.getElementById("i_form");
+const inputBtn = document.getElementById("i_btn");
 
 function insertName() {
-  let selectedItem = itemList.options[itemList.selectedIndex].text;
+  const selectedItem = itemList.options[itemList.selectedIndex].text;
   inputForm.value = selectedItem;
+  inputColor();
 }
 
 function saveBtn() {
   if (!inputForm.value.trim()) {
-    inputForm.style.border = "2px solid red";
+    inputForm.classList.add("input-line-invalid");
+    inputBtn.classList.add("input-btn-invalid");
+    inputForm.classList.remove("input-line");
+    inputBtn.classList.remove("input-btn");
   } else {
     itemList.options[itemList.selectedIndex].text = inputForm.value.trim();
     inputForm.value = inputForm.value.trim();
   }
+}
+function inputColor() {
+  inputForm.classList.add("input-line");
+  inputBtn.classList.add("input-btn");
+  inputForm.classList.remove("input-line-invalid");
+  inputBtn.classList.remove("input-btn-invalid");
 }
